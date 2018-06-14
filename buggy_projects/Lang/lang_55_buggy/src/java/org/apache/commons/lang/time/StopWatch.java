9457aca9ba51fc5f7d8fecd54ca9a986823ae0ac
SophieHYe <He_Ye_90s@hotmail.com>
2018-06-14 15:27:44 +0200

    added script to run ase15 tests

--- buggy_projects/Lang/lang_55_buggy/src/java/org/apache/commons/lang/time/StopWatch.java
+++ buggy_projects/Lang/lang_55_buggy/src/java/org/apache/commons/lang/time/StopWatch.java
@@ -115,9 +115,7 @@ public class StopWatch {
         if(this.runningState != STATE_RUNNING && this.runningState != STATE_SUSPENDED) {
             throw new IllegalStateException("Stopwatch is not running. ");
         }
-        if(-1 == org.apache.commons.lang.time.StopWatch.this.stopTime) {
             stopTime = System.currentTimeMillis();
-        }
         this.runningState = STATE_STOPPED;
     }
 
