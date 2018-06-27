6b288977e24f87070fa4bac4b0cd59050e78af4b
SophieHYe <He_Ye_90s@hotmail.com>
2018-06-27 06:06:01 -0700

    update script to run randoop tests

--- autotest.py
+++ autotest.py
@@ -52,33 +52,39 @@ if __name__ == '__main__':
     testType=sys.argv[4] #evosuite or randoop
 
     if projectId=='Lang':
-        target_test_path='./fixed_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_fixed/src/test/java'
+        target_test_path='./buggy_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_buggy/src/test/java'
         if not os.path.isdir(target_test_path):
-                target_test_path='./fixed_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_fixed/src/test'
+                target_test_path='./buggy_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_buggy/src/test'
     if projectId=='Math':
-        target_test_path='./fixed_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_fixed/src/test/java'
+        target_test_path='./buggy_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_buggy/src/test/java'
         if not os.path.isdir(target_test_path):
-                target_test_path='./fixed_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_fixed/src/test'
+                target_test_path='./buggy_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_buggy/src/test'
     if projectId=='Chart':
-        target_test_path='./fixed_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_fixed/tests'
+        target_test_path='./buggy_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_buggy/tests'
     if projectId=='Time':
-        target_test_path='./fixed_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_fixed/src/test/java'
-    program_path='fixed_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_fixed'
+        target_test_path='./buggy_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_buggy/src/test/java'
+    program_path='buggy_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_buggy'
+    
+    if patchType=='correct':
+       patchpath='claimed_correct_patches/'+toolId+'/'+projectId+'/'+patchName 
+    else:
+        patchpath='plausible_patches/'+toolId+'/'+projectId+'/'+patchName
 
     ##Evosuite
     if testType=='evosuite':
         if testgroup=='ASE15':
-                testpath='./automatically_generated_tests/ASE15/evosuite'+projectId+'/'+bugId+'/'+projectId+'/evosuite-branch'
+                testpath='./automatically_generated_tests/ASE15/evosuite/'+projectId+'/'+bugId+'/'+projectId+'/evosuite-branch'
         elif testgroup== 'EMSE18':
                 testpath='./automatically_generated_tests/EMSE18/'+projectId+'/'+bugId+'/'
+        print testpath
         commonpath = commonTestPath(testpath+'/0')
        
-        with open('failing_tests.csv', 'wb') as csvfile:
+        with open('randoop-test.csv', 'a') as csvfile:
             filewriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
             if commonpath=='':
                 print "NO TESTS!"
-                filewriter.writerow([projectId, bugId,testType,0,'No Tests'])
+                filewriter.writerow([patchName,projectId, bugId,testType,0,'No Tests'])
                 sys.exit()
             else:         
                 for i in range(7,10):
@@ -102,35 +108,75 @@ if __name__ == '__main__':
                             failingTestClass=line.split('::')[0]
                             failingInfo=failingInfo+';'+line.split('::')[1]
                             print failingInfo
-                    filewriter.writerow([projectId, bugId, testType, i,failingTestsNo, failingInfo])
+                    filewriter.writerow([patchName,projectId, bugId, testType, i,failingTestsNo, failingInfo])
                     os.chdir('../../../')
                     os.remove(target_test_file)
 
     #Randoop
     elif testType=='randoop':
-        for i in range(1,2):
-            print i
-            original_test_path='./automatically_generated_tests/ASE15/randoop/'+projectId+'/randoop/'+str(i)+'/'+projectId+'-'+bugId+'f-randoop.'+str(i)
-            print original_test_path
-            number=countJavaFile(original_test_path)
-            print number
-            for j in range(0,number):
-                shutil.copyfile(original_test_path+'/RandoopTest'+str(j)+'.java', target_test_path+'/RandoopTest'+str(j)+'.java')
-            os.chdir(program_path)
-            os.system(d4jpath+'/defects4j compile')
-            result=os.popen(d4jpath+'/defects4j test').read()
-            print result
-            os.chdir('../../../')
-            for j in range(0,number):
-                os.remove(target_test_path+'/RandoopTest'+str(j)+'.java')
-
-            
-            
-            # shutil.copyfile(original_test_file, target_test_file)        
-            # os.chdir(program_path)
-            # os.system(d4jpath+'/defects4j compile')
-            # result=os.popen(d4jpath+'/defects4j test').read()
-            # print result
+         with open('randop-test.csv', 'a') as csvfile:
+            filewriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            with open(patchpath) as f:
+                difffiles=f.read().split('\n\n\n')
+                for diffs in difffiles:
+                    if patchType=='correct':
+                        filepath='claimed_correct_patches/'+toolId+'/'+projectId+'/tmp.patch'
+                    else:
+                        filepath='plausible_patches/'+toolId+'/'+projectId+'/tmp.patch'
+                    f=open(filepath,"w")
+                    f.write(diffs)
+                    f.close()
+                    if patchType=='correct':
+                        tmppatch='./claimed_correct_patches/'+toolId+'/'+projectId+'/tmp.patch'
+                    else:
+                        tmppatch='./plausible_patches/'+toolId+'/'+projectId+'/tmp.patch'
+                    first_line = diffs.split('\n')[0]
+                    filepath=first_line.split('--- ')[1]
+                    print filepath
+                    program_path='buggy_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_buggy'
+                    original_file='./'+program_path+filepath
+                    os.system("patch -u -l -i " +tmppatch +" "+ original_file)
+                    print tmppatch
+                    os.remove(tmppatch)
+                    #copy randoop tests
+                    for i in range(1,11):
+                        print i
+                        original_test_path='./automatically_generated_tests/ASE15/randoop/'+projectId+'/randoop/'+str(i)+'/'+projectId+'-'+bugId+'f-randoop.'+str(i)
+                        print original_test_path
+                        number=countJavaFile(original_test_path)
+                        print number
+                        for j in range(0,number):
+                            shutil.copyfile(original_test_path+'/RandoopTest'+str(j)+'.java', target_test_path+'/RandoopTest'+str(j)+'.java')
+                        os.chdir(program_path)
+                        os.system(d4jpath+'/defects4j compile')
+                        result=os.popen(d4jpath+'/defects4j test').read()
+                        print result
+                        print result.split('\n')[0]
+                        filewriter.writerow([patchName,projectId, bugId, testType, i,result.split('\n')[0]])
+                        os.chdir('../../../')
+                    for j in range(0,number):
+                            os.remove(target_test_path+'/RandoopTest'+str(j)+'.java')
+                    #####revert changes
+                for diffs in difffiles:
+                    if patchType=='correct':
+                        filepath='claimed_correct_patches/'+toolId+'/'+projectId+'/tmp.patch'
+                    else:
+                        filepath='plausible_patches/'+toolId+'/'+projectId+'/tmp.patch'
+                    f=open(filepath,"w")
+                    f.write(diffs)
+                    f.close()
+                    if patchType=='correct':
+                        tmppatch='./claimed_correct_patches/'+toolId+'/'+projectId+'/tmp.patch'
+                    else:
+                        tmppatch='./plausible/'+toolId+'/'+projectId+'/tmp.patch' 
+                    first_line = diffs.split('\n')[0]
+                    filepath=first_line.split('--- ')[1]
+                    print filepath
+                    program_path='buggy_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_buggy'
+                    original_file='./'+program_path+filepath
+                    os.system("patch -R -i " +tmppatch +" "+ original_file)
+                    os.remove(tmppatch)
 
 
 
@@ -153,7 +199,7 @@ if __name__ == '__main__':
         
         # print "Automatically Generated Tests compiled successfully"
     
-        # with open(patchpath) as f:
+        # with open() as f:
         #     difffiles=f.read().split('\n\n\n')
         #     for diffs in difffiles:
         #         if patchType=='correct':
