84d92fc5764cee5a401b4ff50cc12d38db4b7914
SophieHYe <He_Ye_90s@hotmail.com>
2018-07-27 07:02:50 -0700

    update test running result

---
+++ automatically_generated_tests/ASE15/randoop/Lang/randoop/10/Lang-57f-randoop.10/RandoopTest0.java
@@ -0,0 +1,374 @@
+
+import junit.framework.*;
+
+public class RandoopTest0 extends TestCase {
+
+  public static boolean debug = false;
+
+  public void test1() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test1"); }
+
+
+    java.util.Locale var0 = null;
+    java.util.Locale var1 = null;
+    java.util.List var2 = org.apache.commons.lang.LocaleUtils.localeLookupList(var0, var1);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var2);
+
+  }
+
+  public void test2() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test2"); }
+
+
+    java.util.Locale var0 = null;
+    boolean var1 = org.apache.commons.lang.LocaleUtils.isAvailableLocale(var0);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == false);
+
+  }
+
+  public void test3() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test3"); }
+
+
+    boolean var1 = org.apache.commons.lang.SystemUtils.isJavaVersionAtLeast((-1));
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == true);
+
+  }
+
+  public void test4() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test4"); }
+
+
+    java.io.File var0 = org.apache.commons.lang.SystemUtils.getUserDir();
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var0);
+
+  }
+
+  public void test5() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test5"); }
+
+
+    java.io.File var0 = org.apache.commons.lang.SystemUtils.getJavaIoTmpDir();
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var0);
+
+  }
+
+  public void test6() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test6"); }
+
+
+    java.io.File var0 = org.apache.commons.lang.SystemUtils.getUserHome();
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var0);
+
+  }
+
+  public void test7() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test7"); }
+
+
+    float var0 = org.apache.commons.lang.SystemUtils.getJavaVersion();
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var0 == 1.7f);
+
+  }
+
+  public void test8() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test8"); }
+
+
+    // The following exception was thrown during execution.
+    // This behavior will recorded for regression testing.
+    try {
+      java.util.Locale var1 = org.apache.commons.lang.LocaleUtils.toLocale("hi!");
+      fail("Expected exception of type java.lang.IllegalArgumentException");
+    } catch (java.lang.IllegalArgumentException e) {
+      // Expected exception.
+    }
+
+  }
+
+  public void test9() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test9"); }
+
+
+    java.util.List var1 = org.apache.commons.lang.LocaleUtils.countriesByLanguage("hi!");
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var1);
+
+  }
+
+  public void test10() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test10"); }
+
+
+    // The following exception was thrown during execution.
+    // This behavior will recorded for regression testing.
+    try {
+      java.util.Locale var1 = org.apache.commons.lang.LocaleUtils.toLocale("");
+      fail("Expected exception of type java.lang.IllegalArgumentException");
+    } catch (java.lang.IllegalArgumentException e) {
+      // Expected exception.
+    }
+
+  }
+
+  public void test11() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test11"); }
+
+
+    java.util.Locale var0 = null;
+    java.util.List var1 = org.apache.commons.lang.LocaleUtils.localeLookupList(var0);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var1);
+
+  }
+
+  public void test12() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test12"); }
+
+
+    java.util.List var0 = org.apache.commons.lang.LocaleUtils.availableLocaleList();
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var0);
+
+  }
+
+  // public void test13() throws Throwable {
+
+  //   if (debug) { System.out.println(); System.out.print("RandoopTest0.test13"); }
+
+
+  //   boolean var0 = org.apache.commons.lang.SystemUtils.isJavaAwtHeadless();
+    
+  //   // Regression assertion (captures the current behavior of the code)
+  //   assertTrue(var0 == false);
+
+  // }
+
+  public void test14() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test14"); }
+
+
+    org.apache.commons.lang.SystemUtils var0 = new org.apache.commons.lang.SystemUtils();
+
+  }
+
+  public void test15() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test15"); }
+
+
+    java.util.Set var0 = org.apache.commons.lang.LocaleUtils.availableLocaleSet();
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var0);
+
+  }
+
+  public void test16() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test16"); }
+
+
+    boolean var1 = org.apache.commons.lang.SystemUtils.isJavaVersionAtLeast(100.0f);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == false);
+
+  }
+
+  public void test17() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test17"); }
+
+
+    boolean var1 = org.apache.commons.lang.SystemUtils.isJavaVersionAtLeast(0.0f);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == true);
+
+  }
+
+  public void test18() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test18"); }
+
+
+    boolean var1 = org.apache.commons.lang.SystemUtils.isJavaVersionAtLeast(1);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == true);
+
+  }
+
+  public void test19() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test19"); }
+
+
+    java.io.File var0 = org.apache.commons.lang.SystemUtils.getJavaHome();
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var0);
+
+  }
+
+  public void test20() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test20"); }
+
+
+    boolean var1 = org.apache.commons.lang.SystemUtils.isJavaVersionAtLeast(1.7f);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == true);
+
+  }
+
+  public void test21() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test21"); }
+
+
+    java.util.List var1 = org.apache.commons.lang.LocaleUtils.languagesByCountry("");
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var1);
+
+  }
+
+  public void test22() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test22"); }
+
+
+    java.util.List var1 = org.apache.commons.lang.LocaleUtils.languagesByCountry("hi!");
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var1);
+
+  }
+
+  public void test23() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test23"); }
+
+
+    org.apache.commons.lang.LocaleUtils var0 = new org.apache.commons.lang.LocaleUtils();
+
+  }
+
+  public void test24() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test24"); }
+
+
+    boolean var1 = org.apache.commons.lang.SystemUtils.isJavaVersionAtLeast(0);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == true);
+
+  }
+
+  public void test25() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test25"); }
+
+
+    java.util.List var1 = org.apache.commons.lang.LocaleUtils.countriesByLanguage("");
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertNotNull(var1);
+
+  }
+
+  public void test26() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test26"); }
+
+
+    boolean var1 = org.apache.commons.lang.SystemUtils.isJavaVersionAtLeast((-1.0f));
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == true);
+
+  }
+
+  public void test27() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test27"); }
+
+
+    boolean var1 = org.apache.commons.lang.SystemUtils.isJavaVersionAtLeast(1.0f);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == true);
+
+  }
+
+  public void test28() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test28"); }
+
+
+    boolean var1 = org.apache.commons.lang.SystemUtils.isJavaVersionAtLeast(100);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == true);
+
+  }
+
+  public void test29() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test29"); }
+
+
+    boolean var1 = org.apache.commons.lang.SystemUtils.isJavaVersionAtLeast(10);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == true);
+
+  }
+
+  public void test30() throws Throwable {
+
+    if (debug) { System.out.println(); System.out.print("RandoopTest0.test30"); }
+
+
+    boolean var1 = org.apache.commons.lang.SystemUtils.isJavaVersionAtLeast(10.0f);
+    
+    // Regression assertion (captures the current behavior of the code)
+    assertTrue(var1 == false);
+
+  }
+
+}
