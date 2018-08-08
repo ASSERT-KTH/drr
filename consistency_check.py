cd594e428508456accda67282604423728d68b53
SophieHYe <He_Ye_90s@hotmail.com>
2018-08-08 09:27:21 -0700

    update consistency check

--- consistency_check.py
+++ consistency_check.py
@@ -1,76 +1,100 @@
 #!/usr/bin/python
-import sys, os, subprocess,fnmatch, filecmp
+import sys, os, subprocess,fnmatch, filecmp,csv
 
-def travFolder(dir):
+def travFolder(dir,type):
    listdirs = os.listdir(dir)
    for f in listdirs:
-       pattern = '*.patch'
+       pattern = 'patch*.patch'
        if os.path.isfile(os.path.join(dir, f)):
            if fnmatch.fnmatch(f, pattern):
-                 consistency_check(f)
-
+                checkout_project(f)
+                consistency_check(f,type)
+                remove_project(f)
        else:
-            travFolder(dir+'/'+f)
+           print f
+           if 'tmp.patch' not in f:
+                travFolder(dir+'/'+f,type)
 
-def  consistency_check(file):
+
+
+def checkout_project(file):
     filename=os.path.splitext(file)[0]
     arraynames=filename.split("-")
-    # filename[0] patch number     
-    # filename[1]  project name, i.e., Lang, Chart...
-    # filename[2]  bug id
-    # filename[3]  repair tool name, i.e. Dynamoth   
+    projectId=arraynames[1]
+    bugId=arraynames[2]
+    proj_lower_cast=projectId.decode('utf-8').lower()
+    if not os.path.exists('tmp_projects'):
+         os.system('mkdir tmp_projects')
+    if not os.path.exists('tmp_projects/'+projectId):
+        os.system('mkdir tmp_projects/'+projectId)
+    os.system( d4jpath+'/defects4j checkout -p '+projectId+' -v '+bugId+'b -w ./tmp_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_buggy')  
+
+
+
+
+def remove_project(file):
+    filename=os.path.splitext(file)[0]
+    arraynames=filename.split("-")
+    projectId=arraynames[1]
+    bugId=arraynames[2]
+    if  os.path.exists('tmp_projects'):
+        os.system('rm -rf tmp_projects')
+
+
+
+def consistency_check(file,type):
+    filename=os.path.splitext(file)[0]
+    arraynames=filename.split("-")
+    projectId=arraynames[1]
+    bugId=arraynames[2]
+    toolId=arraynames[3]
     currentpath=os.path.dirname(os.path.realpath(__file__))
-    proj_lower_cast=arraynames[1].decode('utf-8').lower()
+    proj_lower_cast=projectId.decode('utf-8').lower()
     #get patch path
-    patchpath='patches/'+arraynames[3]+'/'+arraynames[1]+'/'+file 
+    if type=='correct':
+        patchpath='claimed_correct_patches/'+toolId+'/'+projectId+'/'+file 
+    else:
+         patchpath='plausible_patches/'+toolId+'/'+projectId+'/'+file
     with open(patchpath) as f:
-        difffiles=f.read().split('\n\n')
+        difffiles=f.read().split('\n\n\n')
         for diffs in difffiles:
-            f=open('patches/'+arraynames[3]+'/'+arraynames[1]+'/tmp.patch',"w")
+            if type=='correct':
+                filepath='claimed_correct_patches/'+toolId+'/'+projectId+'/tmp.patch'
+            else:
+                filepath='plausible_patches/'+toolId+'/'+projectId+'/tmp.patch'
+            f=open(filepath,"w")
             f.write(diffs)
             f.close()
-            tmppatch='./patches/'+arraynames[3]+'/'+arraynames[1]+'/tmp.patch' 
+            if type=='correct':
+                tmppatch='./claimed_correct_patches/'+toolId+'/'+projectId+'/tmp.patch'
+            else:
+                tmppatch='./plausible_patches/'+toolId+'/'+projectId+'/tmp.patch'
             first_line = diffs.split('\n')[0]
             filepath=first_line.split('--- ')[1]
-            names=first_line.split('/')
-            target_file=names[len(names)-1]
-            program_path='buggy_projects/'+arraynames[1]+'/'+proj_lower_cast+'_'+arraynames[2]+'_buggy'
+            print filepath
+            program_path='tmp_projects/'+projectId+'/'+proj_lower_cast+'_'+bugId+'_buggy'
             original_file='./'+program_path+filepath
-            patched_file='patched_programs/'+arraynames[3]+'/'+arraynames[1]+'/bug_'+arraynames[2]+'/'+arraynames[0]+'/'+target_file
-            patch_command="diff  -b -u " + original_file +"   "+  patched_file  +" > tmpdiff.patch"
-            os.system(patch_command)
-            if not compare_two_files(tmppatch,"tmpdiff.patch"):
-                print '*********patch file '+filename+' is INCONSISTENT!'          
-            else:
-                print file+' consistency check PASS!'
-            os.remove(tmppatch)
-            os.remove("tmpdiff.patch")
-
-
-
-def compare_two_files(tmppatchpath,tmpdiffpath):
-    
-    with open(tmppatchpath) as file1, open(tmpdiffpath) as file2:
-        n=0
-        for line1, line2 in zip(file1,file2):
-            n=n+1
-            if(n>2):
-                if line1 != line2:
-                    return False
+            result=os.popen("patch -u -l --fuzz=10  -i  " +tmppatch +" "+ original_file).read()
+            print result
+            #record the consistency result in csv file
+            with open('./tables/consistency_check.csv', 'a') as csvfile:
+                filewriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                if "FAILED" in result:
+                    filewriter.writerow([file,type,"FAILED"])
                 else:
-                    return True
-            
-                    
-    
-    file1.close()
-    file2.close()
+                    filewriter.writerow([file,type,"OK"])
 
-
+            os.remove(tmppatch)         
+                           
 
       
    
 if __name__ == '__main__':
-    folderdir='./patches/ACS/Math'
-    travFolder(folderdir)
-
+    currentpath=os.path.dirname(os.path.realpath(__file__))
+    d4jpath=currentpath+'/defects4j/framework/bin'
+    # folderdir1='./claimed_correct_patches'
+    # travFolder(folderdir1,'correct')
+    folderdir2='./plausible_patches/JGenProg2017'
+    travFolder(folderdir2,'plausible')
 
